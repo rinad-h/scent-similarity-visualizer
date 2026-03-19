ae2c106d9dd49a16cce7d1b244bc7ee8ae6192ee
noorsyed16 <noorsyed464@gmail.com>
2026-03-19 11:00:16 -0600

    Creating LoginPage.css, LoginPage.js and updating App.js
    Created the 2 files needed for the frontend and updates the routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import SimilarityFeature from "./features/similarity/SimilarityFeature";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/discover" element={<SimilarityFeature />} />
       </Routes>
     </Router>
